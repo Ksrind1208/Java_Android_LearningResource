719188f68e1a4d62d87874f9d2eaecd2db37eecc
Ksrind1208 <minhducm1208@gmail.com>
2024-08-20 21:49:47 +0700

    Add new file

---
+++ FragmentActivityWithTabLayout/app/src/main/java/com/example/fragmentactivity/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.fragmentactivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+    TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        frameLayout=findViewById(R.id.frameLayout);
+        tabLayout=findViewById(R.id.tabLayout);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment=null;
+
+                if(tab.getPosition()==0){
+                    fragment=new FirstFragment();
+                }
+                if(tab.getPosition()==1){
+                    fragment=new SecondFragment();
+                }
+                if(tab.getPosition()==2){
+                    fragment=new ThirdFragment();
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,fragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
